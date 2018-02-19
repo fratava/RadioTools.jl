9b552d14f8fd2f7cf808a43713056cf59420cd37
Francisco Tapia <ftapia.va@gmail.com>
2018-02-19 12:10:30 -0600

    0.0.8

--- src/convert.jl
+++ src/convert.jl
@@ -4,26 +4,53 @@ fre2wl(ν) = c / ν #cm
 wl2fre(λ) = c / λ #Hz
 
 #Distance
-m2cm(m)     = m   * 100
+function m2cm(m)
+       return m   * 100
+end
 
-mm2cm(mm)   = mm  * 0.1
-mm2m(mm)    = mm  * 0.001
-mm2um(mm)   = mm  * 1000
+function mm2cm(mm)
+       return mm  * 0.1
+end
 
-cm2m(cm)    = cm  * 0.01
-cm2mm(cm)   = cm  * 10
+function mm2m(mm)
+       return mm  * 0.001
+end
+
+function mm2um(mm)
+       return mm  * 1000
+end
+
+function cm2m(cm)
+       return cm  * 0.01
+end
+
+function cm2mm(cm)
+       return cm  * 10
+end
 #Frequency
-hz2ghz(hz)  = hz  / 1e9
-hz2mhz(hz)  = hz  / 1e6
+function hz2ghz(hz)
+       return hz  / 1e9
+end
 
-ghz2hz(ghz) = ghz * 1e9
-mhz2hz(mhz) = mhz * 1e6
+function hz2mhz(hz)
+       return hz  / 1e6
+end
+
+function ghz2hz(ghz)
+       return ghz * 1e9
+end
+
+function mhz2hz(mhz)
+       return mhz * 1e6
+end
 #Angle
-arcs2dg(arcs) = arcs * 0.00028
+function arcs2dg(arcs)
+       return arcs * 0.00028
+end
 
 export fre2wl, wl2fre,
        m2cm,
-       mm2cm, mm2m, mm2um 
+       mm2cm, mm2m, mm2um
        cm2m, cm2mm,
        hz2ghz, hz2mhz,
        ghz2hz, mhz2hz,
