bc20be2a77e0b7aa48a16b1b2950a723394ad054
Francisco Tapia <ftapia.va@gmail.com>
2018-02-19 19:15:37 -0600

    0.0.9.4

--- src/constants.jl
+++ src/constants.jl
@@ -1,5 +1,15 @@
+#CGS
+
 const c           = 2.997924561e10 #cm s⁻¹
-const au          = 1.49597892e12 #cm
-const solar_radio = 6.9598e10 #cm
+const au          = 1.49597892e12  #cm
+const solar_mass  = 1.989e33       #g
+const solar_lum   = 3.826e33       #erg s⁻¹
+const solar_radio = 6.9598e10      #cm
+const h           = 6.62619650     #erg s
 const k_b         = 1.38064852e-17 #erg K⁻¹
-const a = 12.3
+const pc          = 3.085677582e18 #cm
+const Mpc         = 3.085677582e24 #cm
+const ly          = 9.46073047e17  #cm
+const Jy          = 1e-23          #erg cm⁻² s⁻¹ Hz⁻¹
+const sfu         = 1e-19          #erg cm⁻² s⁻¹ Hz⁻¹
+const σ           = 5.6696196e-5   #erg cm⁻² s⁻¹ K⁻⁴
