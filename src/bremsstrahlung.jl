0a75026130bc58e1e2efbe301f60ad21f9f3a17c
Francisco Tapia <ftapia.va@gmail.com>
2018-05-22 15:08:20 -0500

    0.2.5.7.2

--- src/bremsstrahlung.jl
+++ src/bremsstrahlung.jl
@@ -14,15 +14,15 @@ function bremsstrahlung(N, T, ν)
     if T <= 2e5
         A = 18.2 + 3/2 * log(T) - log(ν)
     else
-#        A = 24.5 + log(T) - log(ν)
+        A = 24.5 + log(T) - log(ν)
     end
 
-    κ1 = 9.78e-3 * (Ne^2 / (ν^2 * T^(3/2))) * A
+    κ = 9.78e-3 * (Ne^2 / (ν^2 * T^(3/2))) * A
 
-    κ2 = 1.3705e-35 * T^(0.5) * Ne * Ne * ν^(-3) * (1 - exp(-heV * ν / (keV * T) )) * gaunt
+    #κ2 = 1.3705e-35 * T^(0.5) * Ne * Ne * ν^(-3) * (1 - exp(-heV * ν / (keV * T) )) * gaunt
     #κ = 3.7e8 * T^(0.5) * Ne * HeIII * ν^(-3) * (1 - exp(-heV * ν / (keV * T) )) * gaunt
 
-    return (κ1 + κ2)
+    return k
 end
 
 export bremsstrahlung
