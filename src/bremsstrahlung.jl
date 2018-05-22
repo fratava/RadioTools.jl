cfced543533c7659059b4625647fe51cb5f6d99d
Francisco Tapia <ftapia.va@gmail.com>
2018-05-21 20:49:15 -0500

    0.2.3.3.2
    Issue

--- src/bremsstrahlung.jl
+++ src/bremsstrahlung.jl
@@ -7,9 +7,9 @@ function bremsstrahlung(N, T, ν)
     Ne = saha(T, N)
 
     if T < 2e5
-        A = 18.2 + log(T^(3/2)) - log(ν)
+        A = 18.2 #+ log(T^(3/2)) - log(ν)
     else
-        A = 24.5 + log(T) - log(ν)
+        A = 24.5 #+ log(T) - log(ν)
     end
 
     κ = 9.78e-3 * (Ne^2 / (ν^2 * T^(3/2))) * A
