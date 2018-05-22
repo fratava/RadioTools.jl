2eb1155eb04469664c2f785681da5c1489b1f6a9
Francisco Tapia <ftapia.va@gmail.com>
2018-05-22 15:05:55 -0500

    0.2.5.7

--- src/bremsstrahlung.jl
+++ src/bremsstrahlung.jl
@@ -11,18 +11,18 @@ function bremsstrahlung(N, T, ν)
 
     Ne , HI, HII, HeI, HeII, HeIII = saha(T, N)
 
-    #if T <= 2e5
-    #    A = 18.2 + 3/2 * log(T) - log(ν)
-    #else
+    if T <= 2e5
+        A = 18.2 + 3/2 * log(T) - log(ν)
+    else
 #        A = 24.5 + log(T) - log(ν)
-    #end
+    end
 
-    #κ = 9.78e-3 * (Ne * (HI + HII + HeI + HeII + HeIII) / (ν^2 * T^(3/2))) * A
+    κ1 = 9.78e-3 * (Ne^2 / (ν^2 * T^(3/2))) * A
 
-    κ = 1.3705e-35 * T^(0.5) * Ne * Ne * ν^(-3) * (1 - exp(-heV * ν / (keV * T) )) * gaunt
+    κ2 = 1.3705e-35 * T^(0.5) * Ne * Ne * ν^(-3) * (1 - exp(-heV * ν / (keV * T) )) * gaunt
     #κ = 3.7e8 * T^(0.5) * Ne * HeIII * ν^(-3) * (1 - exp(-heV * ν / (keV * T) )) * gaunt
 
-    return κ
+    return (κ1 + κ2) 
 end
 
 export bremsstrahlung
