c25fba19ef56b8c73250727ded3c9bd2636aa243
Francisco Tapia <ftapia.va@gmail.com>
2018-05-22 13:37:47 -0500

    0.2.4.8
    Issue

--- src/physics.jl
+++ src/physics.jl
@@ -15,7 +15,7 @@ function intensity(N, ν, T, D, I0)
     for i=2:length(N)
 
         Δx  = D[i] - D[i-1]
-        temp = τ([N[i-1] N[i]],[T[i-1] T[i]], ν, Δx)
+        ttemp = τ([N[i-1] N[i]],[T[i-1] T[i]], ν, Δx)
         push!(tau, ttemp)
 
         I_temp = I[i-1] * exp(-τ([N[i-1] N[i]],[T[i-1] T[i]], ν, Δx)) +
